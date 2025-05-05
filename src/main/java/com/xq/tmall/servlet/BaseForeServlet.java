89633131f32c4fae98794eca38942f40cd2462f6
wingloong <wingloong921@gmail.com>
2025-05-05 22:46:38 +0800

    create tmall repo

---
+++ src/main/java/com/xq/tmall/servlet/BaseForeServlet.java
@@ -0,0 +1,83 @@
+/**
+* 模仿天猫整站j2ee 教程 为how2j.cn 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
+
+package com.xq.tmall.servlet;
+
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.xq.tmall.dao.CategoryDAO;
+import com.xq.tmall.dao.OrderDAO;
+import com.xq.tmall.dao.OrderItemDAO;
+import com.xq.tmall.dao.ProductDAO;
+import com.xq.tmall.dao.ProductImageDAO;
+import com.xq.tmall.dao.PropertyDAO;
+import com.xq.tmall.dao.PropertyValueDAO;
+import com.xq.tmall.dao.ReviewDAO;
+import com.xq.tmall.dao.UserDAO;
+import com.xq.tmall.util.Page;
+
+public class BaseForeServlet extends HttpServlet{
+
+	protected CategoryDAO categoryDAO = new CategoryDAO();
+	protected OrderDAO orderDAO = new OrderDAO();
+	protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+	protected ProductDAO productDAO = new ProductDAO();
+	protected ProductImageDAO productImageDAO = new ProductImageDAO();
+	protected PropertyDAO propertyDAO = new PropertyDAO();
+	protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+	protected ReviewDAO reviewDAO = new ReviewDAO();
+	protected UserDAO userDAO = new UserDAO();
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			
+			int start= 0;
+			int count = 10;
+			try {
+				start = Integer.parseInt(request.getParameter("page.start"));
+			} catch (Exception e) {
+				
+			}
+			
+			try {
+				count = Integer.parseInt(request.getParameter("page.count"));
+			} catch (Exception e) {
+			}
+			
+			Page page = new Page(start,count);
+			
+			String method = (String) request.getAttribute("method");
+
+			Method m = this.getClass().getMethod(method, javax.servlet.http.HttpServletRequest.class,
+					javax.servlet.http.HttpServletResponse.class,Page.class);
+			
+			String redirect = m.invoke(this,request, response,page).toString();
+			
+			if(redirect.startsWith("@"))
+				response.sendRedirect(redirect.substring(1));
+			else if(redirect.startsWith("%"))
+				response.getWriter().print(redirect.substring(1));
+			else
+				request.getRequestDispatcher(redirect).forward(request, response);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+}
+
+/**
+* 模仿天猫整站j2ee 教程 为how2j.cn 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
